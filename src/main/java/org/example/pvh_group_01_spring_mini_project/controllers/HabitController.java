63e47548a6696265734c0a8e9f4272b7c08cecd2
Phallin <you@example.com>
2025-04-05 00:31:41 +0700

    test git

--- src/main/java/org/example/pvh_group_01_spring_mini_project/controllers/HabitController.java
+++ src/main/java/org/example/pvh_group_01_spring_mini_project/controllers/HabitController.java
@@ -1,9 +1,14 @@
 package org.example.pvh_group_01_spring_mini_project.controllers;
 
+import org.example.pvh_group_01_spring_mini_project.models.dto.request.HabitRequest;
+import org.example.pvh_group_01_spring_mini_project.models.entity.Habit;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/habits")
 public class HabitController {
+    public Habit deleteHabit(HabitRequest habitRequest) {
+        return null;
+    }
 }
