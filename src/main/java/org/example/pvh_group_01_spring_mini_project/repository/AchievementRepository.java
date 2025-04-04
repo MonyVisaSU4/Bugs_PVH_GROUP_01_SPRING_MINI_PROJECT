27c9e73640bcd9130996b5215b9a11ecbf4db17d
MonyVisaSU4 <visagoodboy042@gmail.com>
2025-04-04 23:05:54 +0700

    OtherCommit

--- src/main/java/org/example/pvh_group_01_spring_mini_project/repository/AchievementRepository.java
+++ src/main/java/org/example/pvh_group_01_spring_mini_project/repository/AchievementRepository.java
@@ -1,7 +1,16 @@
 package org.example.pvh_group_01_spring_mini_project.repository;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface AchievementRepository {
+//    @Select("""
+//        SELECT * FROM achievement;
+//    """)
+//    @Results(id = "AchievementMapper", value = {
+//            @Result(property = "achievementId", column = "achievement_id"),
+//    })
 }
